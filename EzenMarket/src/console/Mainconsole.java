cf2bfca329a4984ab94f8789d3094d577dbea5b0
Cripre <farenhite97@naver.com>
2025-07-07 09:53:08 +0900

    Start prject

--- EzenMarket/src/console/Mainconsole.java
+++ EzenMarket/src/console/Mainconsole.java
@@ -1,7 +1,14 @@
 package console;
 
+import java.util.Scanner;
+
 public class Mainconsole extends Thread{
+    Scanner sc = new Scanner(System.in);
     @Override
     public void run() {
+        while(true){
+            System.out.println("> ");
+            sc.nextLine();
+        }
     }
 }
